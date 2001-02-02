33ac35b60c1c102003f6a4a5774937c922c92657
johans <none@none>
2001-02-02 13:30:42 +0000

    Fake a setgroups() function where needed (Winsucks)

---
+++ src/setgroups.c
@@ -0,0 +1,11 @@
+/* Copyright (C) 1995, 1996 by Sven Berkvens (sven@stack.nl) */
+
+#include	"config.h"
+#include	<sys/types.h>
+
+extern	int
+setgroups DECL2(int, ngroups, const gid_t, *gidset)
+{
+	/* fake success */
+	return 0;
+}
