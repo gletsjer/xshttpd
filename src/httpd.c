02a85c475e0b069e365809ab814fa5b866dbf5a2
johans <none@none>
2003-01-03 18:34:45 +0000

    Source doc updates

--- src/httpd.c
+++ src/httpd.c
@@ -1,6 +1,6 @@
 /* Copyright (C) 1995, 1996 by Sven Berkvens (sven@stack.nl) */
 
-/* $Id: httpd.c,v 1.98 2003/01/03 18:18:11 johans Exp $ */
+/* $Id: httpd.c,v 1.99 2003/01/03 18:34:46 johans Exp $ */
 
 #include	"config.h"
 
@@ -101,7 +101,7 @@ extern	int	setpriority PROTO((int, int, int));
 
 #ifndef		lint
 static char copyright[] =
-"$Id: httpd.c,v 1.98 2003/01/03 18:18:11 johans Exp $ Copyright 1993-2002 Sven Berkvens, Johan van Selst";
+"$Id: httpd.c,v 1.99 2003/01/03 18:34:46 johans Exp $ Copyright 1995-2003 Sven Berkvens, Johan van Selst";
 #endif
 
 /* Global variables */
