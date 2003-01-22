63dc5ba582545bc7735ffd3c331601825d356d83
johans <none@none>
2003-01-22 19:55:36 +0000

    fix cgi perms for linux

--- src/cgi.c
+++ src/cgi.c
@@ -1,5 +1,5 @@
 /* Copyright (C) 1995, 1996 by Sven Berkvens (sven@stack.nl) */
-/* $Id: cgi.c,v 1.74 2003/01/22 17:37:22 johans Exp $ */
+/* $Id: cgi.c,v 1.75 2003/01/22 19:55:36 johans Exp $ */
 
 #include	"config.h"
 
@@ -303,11 +303,20 @@ do_script DECL5(const char *, path, const char *, base, const char *, file, cons
 		for (count = 3; count < 64; count++)
 			close(count);
 
-#ifndef		__linux__
-		/* Not required for linux where uid is fixed */
 		if (!origeuid)
 		{
-			if (setuid(geteuid()) || setgid(getegid()))
+			/* Set uid first, euid later! */
+			uid_t uid = geteuid();
+			gid_t gid = getegid();
+
+			/* mind you, we need to do this as root */
+			seteuid(origeuid);
+			setegid(origegid);
+			setgid(gid);
+			setegid(gid);
+			setuid(uid);
+			seteuid(uid);
+			if (uid != geteuid() || gid != getegid())
 			{
 				secprintf("Content-type: text/plain\r\n\r\n");
 				secprintf("[Invalid UID setting]\n");
@@ -318,7 +327,6 @@ do_script DECL5(const char *, path, const char *, base, const char *, file, cons
 				exit(1);
 			}
 		}
-#endif		/* __linux__ */
 		setenv("PATH", SCRIPT_PATH, 1);
 		if (chdir(base))
 		{
