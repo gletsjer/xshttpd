38d10d64e11c6b8372682bf19398d97d65c4d4f5
johans <none@none>
2004-05-24 18:50:30 +0000

    hide .[Rr]edir files

--- src/methods.c
+++ src/methods.c
@@ -1,5 +1,5 @@
 /* Copyright (C) 1995, 1996 by Sven Berkvens (sven@stack.nl) */
-/* $Id: methods.c,v 1.103 2004/05/07 14:26:14 johans Exp $ */
+/* $Id: methods.c,v 1.104 2004/05/24 18:50:30 johans Exp $ */
 
 #include	"config.h"
 
@@ -636,7 +636,7 @@ do_get DECL1(char *, params)
 		wasdir = (file[strlen(file) - 1] == '/');
 	else
 		wasdir = 0;
-	if (strstr(file, "/..") || strstr(file, "/.xs") || strstr(file, "/.noxs"))
+	if (strstr(file, "/..") || strstr(file, "/.xs") || strstr(file, "/.noxs") || strstr(file, ".redir") || strstr(file, ".Redir"))
 	{
 		server_error("403 Invalid path specified", "INVALID_PATH");
 		return;
