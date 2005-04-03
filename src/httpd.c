246a34b4abaa4fab021440b0853a69fcfa3e24de
johans <none@none>
2005-04-03 17:14:11 +0000

    Include LDAP patch by rink

--- src/httpd.c
+++ src/httpd.c
@@ -1,6 +1,6 @@
 /* Copyright (C) 1995, 1996 by Sven Berkvens (sven@stack.nl) */
 
-/* $Id: httpd.c,v 1.174 2005/04/03 16:52:28 johans Exp $ */
+/* $Id: httpd.c,v 1.175 2005/04/03 17:14:11 johans Exp $ */
 
 #include	"config.h"
 
@@ -68,6 +68,9 @@
 #ifdef		HAVE_MEMORY_H
 #include	<memory.h>
 #endif		/* HAVE_MEMORY_H */
+#ifdef		AUTH_LDAP
+#include	<ldap.h>
+#endif		/* AUTH_LDAP */
 
 #include	"httpd.h"
 #include	"decode.h"
@@ -105,7 +108,7 @@ typedef	size_t	socklen_t;
 
 #ifndef		lint
 static char copyright[] =
-"$Id: httpd.c,v 1.174 2005/04/03 16:52:28 johans Exp $ Copyright 1995-2005 Sven Berkvens, Johan van Selst";
+"$Id: httpd.c,v 1.175 2005/04/03 17:14:11 johans Exp $ Copyright 1995-2005 Sven Berkvens, Johan van Selst";
 #endif
 
 /* Global variables */
@@ -962,6 +965,188 @@ redirect(const char *redir, int permanent)
 	fflush(stdout);
 }
 
+#ifdef AUTH_LDAP
+int
+check_group (LDAP* ld, char* ldapdn, char* user, char* group)
+{
+	LDAPMessage*	res = NULL;
+	LDAPMessage*	e;
+	BerElement*	ber = NULL;
+	char		filter[MYBUFSIZ];
+	char*		a;
+	char**		vals;
+	char*		attrs[] = { "memberUid", NULL };
+	int		result = 0, i;
+
+	/*
+	 * Search for the group first. Most directory have seperate branches
+	 * for users/groups.
+	 */
+	snprintf (filter, MYBUFSIZ, "(cn=%s)", group);
+
+	if (ldap_search_s (ld, ldapdn, LDAP_SCOPE_SUBTREE, filter, attrs, 0, &res) != LDAP_SUCCESS)
+		goto leave;
+	e = ldap_first_entry (ld, res);
+	if (e == NULL)
+		goto leave;
+
+
+	/*
+	 * Look through all retrieved attributes (we only ask for member names,
+	 * so we can just check any attribute for the username)
+	 */
+	for (a = ldap_first_attribute (ld, e, &ber); a != NULL;
+	     a = ldap_next_attribute (ld, e, ber))
+	{
+		vals = ldap_get_values (ld, e, a);
+		if (vals != NULL)
+		{
+			for (i = 0; vals[i] != NULL; i++)
+			{
+				if (!strcasecmp (vals[i], user))
+					result++;
+			}
+
+			ldap_value_free (vals);
+		}
+
+		ldap_memfree (a);
+	}
+
+leave:
+
+	if (res) ldap_msgfree (res);
+	if (ber) ber_free (ber, 0);
+
+	return result;
+}
+
+int
+check_auth_ldap(FILE* authfile, char* user, char* pass)
+{
+	char	ldapuri[MYBUFSIZ];
+	char	ldapdn[MYBUFSIZ];
+	char	ldapattr[MYBUFSIZ];
+	char	ldapgroups[MYBUFSIZ];
+	char	line[MYBUFSIZ];
+	int	ldapversion;
+	char	filter[MYBUFSIZ];
+	char*	dn = NULL;
+	char*	ptr;
+	char*	curoffs;
+	LDAP*	ld;
+	LDAPMessage* res = NULL;
+	LDAPMessage* e;
+	int	ok = 1;
+
+	strcpy (ldapuri, ""); strcpy (ldapdn, ""); strcpy (ldapattr, "uid");
+	strcpy (ldapgroups, ""); ldapversion = 3;
+
+	/* LDAP may support empty passwords to do an anonymous bind. That's
+	 * not what our idea of security is ... */
+	if (!strlen (pass))
+		return(1);
+
+	/*
+ 	 * Quick 'n dirty parser; usually the .xsauth file consists of
+ 	 * Uuser:hash entries. By accepting parameter=value entries, we
+ 	 * won't clash with that (since check_auth() will happily skip
+ 	 * over them)
+ 	 */
+	rewind(authfile);
+	while (fgets(line, MYBUFSIZ, authfile))
+	{
+		/* kill newlines and such, they confuse ldap */
+		while ((ptr = strchr (line, '\n')) != NULL) *ptr = 0;
+		while ((ptr = strchr (line, '\r')) != NULL) *ptr = 0;
+		if (!strncasecmp ("ldaphost=", line, 9))
+			sprintf (ldapuri, "ldap://%s", (line + 9));
+		if (!strncasecmp ("ldapattr=", line, 9))
+			strcpy (ldapattr, (line + 9));
+		if (!strncasecmp ("ldapuri=", line, 8))
+			strcpy (ldapuri, (line + 8));
+		if (!strncasecmp ("ldapdn=", line, 7))
+			strcpy (ldapdn, (line + 7));
+		if (!strncasecmp ("ldapversion=", line, 12))
+			ldapversion = atoi (line + 12);
+		if (!strncasecmp ("ldapgroups=", line, 11))
+			strcpy (ldapgroups, (line + 11));
+	}
+
+	if ((!strlen (ldapuri)) || (!strlen(ldapdn)) || (!strlen (ldapattr)))
+	{
+		/* LDAP config is incomplete */
+		return(1);
+	}
+
+	if (ldap_initialize (&ld, ldapuri) != LDAP_SUCCESS)
+		return(1);
+	ldap_set_option (ld, LDAP_OPT_PROTOCOL_VERSION, &ldapversion);
+
+	/*
+	 * This search may look confusing. Basically, we do a search for the
+	 * user in the tree given, _including all subtrees_.
+	 */
+	snprintf (filter, MYBUFSIZ - 1, "(%s=%s)", ldapattr, user);
+
+	if (ldap_search_s (ld, ldapdn, LDAP_SCOPE_SUBTREE, filter, NULL, 0, &res) != LDAP_SUCCESS)
+		goto leave;
+  
+	/* simply grab the first item */
+	e = ldap_first_entry (ld, res);
+	if (e == NULL)
+		goto leave;
+
+	dn = ldap_get_dn (ld, e);
+	if (dn == NULL)
+		goto leave;
+
+	/* the bind is the actual login, and verifies our password */ 
+	if (ldap_bind_s (ld, dn, pass, LDAP_AUTH_SIMPLE) != LDAP_SUCCESS)
+		goto leave;
+
+	if (!strcmp (ldapgroups, ""))
+	{
+		/* no groups specified, so it's a definite go */
+		ok = 0;
+	}
+	else
+	{
+		curoffs = ldapgroups;
+		while (1)
+		{
+			/* isolate a group on a ',' boundery */
+			ptr = strchr (curoffs, ',');
+			if (ptr == NULL)
+				ptr = strchr (curoffs, 0);
+			strncpy (line, curoffs, (ptr - curoffs));
+			line[(ptr - curoffs)] = 0;
+
+			if (check_group (ld, ldapdn, user, line))
+			{
+				ok = 0;
+				break;
+			}
+
+			if (!*ptr)
+				break;
+			curoffs = ptr + 1;
+		}
+	}
+
+	/* only close file if ldap is successful */
+	if (!ok) fclose (authfile);
+
+leave:
+	if (dn) ldap_memfree (dn);
+	if (res) ldap_msgfree (res);
+	ldap_unbind (ld);
+
+	return ok;
+
+}
+#endif /* AUTH_LDAP */
+
 int
 check_auth(FILE *authfile)
 {
@@ -995,6 +1180,17 @@ check_auth(FILE *authfile)
 		*find++ = 0;
 		setenv("REMOTE_USER", search, 1);
 		setenv("REMOTE_PASSWORD", find, 1);
+
+#ifdef AUTH_LDAP
+		/*
+		 * Try to do an LDAP auth first. This is because xs_encrypt()
+		 * may alter the buffer, in which case we compare garbage.
+		 */
+		if (!check_auth_ldap(authfile, search, find))
+			return(0);
+		rewind (authfile);
+#endif /* AUTH_LDAP */
+
 		snprintf(line, MYBUFSIZ, "%s:%s\n", search, xs_encrypt(find));
 	}
 	while (fgets(compare, MYBUFSIZ, authfile))
