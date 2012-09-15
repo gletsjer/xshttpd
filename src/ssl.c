861d36100eecb6a14edc892cab0c1f8be33146c6
Johan van Selst <johans@gletsjer.net>
2012-09-15 10:47:53 +0200

    - Disable TLS compression (mitigates against CRIME attack) - Set a sensible default set of TLS ciphers

--- src/ssl.c
+++ src/ssl.c
@@ -422,6 +422,8 @@ loadssl(struct socket_config * const lsock, struct ssl_vhost * const sslvhost)
 	if (lsock->sslcipherlist &&
 			!SSL_CTX_set_cipher_list(ssl_ctx, lsock->sslcipherlist))
 		errx(1, "No suitable SSL cipher: %s", lsock->sslcipherlist);
+	else
+		SSL_CTX_set_cipher_list(ssl_ctx, "HIGH:MEDIUM:!aNULL");
 
 #ifdef		SSL_MODE_AUTO_RETRY
 	SSL_CTX_set_mode(ssl_ctx,
@@ -534,7 +536,12 @@ loadssl(struct socket_config * const lsock, struct ssl_vhost * const sslvhost)
 		EC_KEY_free(ecdh);
 	}
 #endif		/* OPENSSL_EC_NAMED_CURVE */
+#ifdef		SSL_OP_NO_COMPRESSION
+	(void)SSL_CTX_set_options(ssl_ctx, SSL_OP_NO_SSLv2 |
+			SSL_OP_NO_COMPRESSION);
+#else		/* SSL_OP_NO_COMPRESSION */
 	(void)SSL_CTX_set_options(ssl_ctx, SSL_OP_NO_SSLv2);
+#endif		/* SSL_OP_NO_COMPRESSION */
 
 	switch (lsock->sslauth)
 	{
